de014dc43da2dc91f1bfe805b80d9bd346b5079e
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-07 15:02:26 -0700

    added get_main_dish method

--- dish.rb
+++ dish.rb
@@ -2,5 +2,10 @@ require "pry"
 require "colorize"
 
 class Dish
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 
 end
--- main.rb
+++ main.rb
@@ -1,29 +1,35 @@
 require "pry"
 require "colorize"
 
+require_relative "dish"
+
 class RubyLunchLady
 
   def initialize
     @order = []
-    @main_dishes = ["Hamburger"]
-    puts "Welcome to Ruby's what can I get for you?"
-    puts "--- Main Dishes ---"
-
-    @main_dishes.each_with_index do |main_dish, index|
-      puts "#{index + 1}. #{main_dish}"
-    end
-
-    print "Make a selection: >"
-    @order << @main_dishes[gets.to_i - 1]
-
+    @main_dishes = [Dish.new("Hamburger")]
+    puts "--- Welcome to Ruby's ---".colorize(:red)
+    get_main_dish
+    
     puts "You ordered: "
-
-    @order.each do |item|
-      puts item
+    
+    @order.each do |dish|
+      puts dish.name
     end
-
-    puts "Have a nice day!"
-
+    
+    puts "Have a nice day!" 
+  end
+  
+  def get_main_dish
+    puts "What can I get for you?"
+    puts "--- Main Dishes ---"
+  
+    @main_dishes.each_with_index do |main_dish, index|
+      puts "#{index + 1}. #{main_dish.name}"
+    end
+  
+    print "Make a selection: >"
+    @order << @main_dishes[gets.to_i - 1]    
   end
 
 end
