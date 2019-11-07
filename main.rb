dd5123e5a87ea6e44f0c194c11e845471cedb65b
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-07 15:14:09 -0700

    added get_side_dish method

--- main.rb
+++ main.rb
@@ -8,11 +8,11 @@ class RubyLunchLady
   def initialize
     @order = []
     @main_dishes = [Dish.new("Hamburger")]
+    @side_dishes = [Dish.new("French Fries")]
     puts "--- Welcome to Ruby's ---".colorize(:red)
     get_main_dish
-    get_order
-    
-    
+    get_side_dish
+    get_order  
   end
   
   def get_main_dish
@@ -25,6 +25,18 @@ class RubyLunchLady
     
     print "Make a selection: >"
     @order << @main_dishes[gets.to_i - 1]    
+  end
+
+  def get_side_dish
+    puts "Your meal comes with 2 sides."
+    puts "--- Side Dishes ---"
+
+    @side_dishes.each_with_index do |dish, index|
+      puts "#{index + 1}. #{dish.name}"
+    end
+
+    print "Make a selection: >"
+    @order << @side_dishes[gets.to_i - 1]
   end
   
   def get_order
